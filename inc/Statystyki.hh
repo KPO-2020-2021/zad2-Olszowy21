502699816fd525f0460daabced73292ffdeca2d0
Kamilos21 <262757@student.pwr.edu.pl>
2021-03-20 21:17:11 +0100

    Zrobiona funkcja zliczająca błędne i poprawne testy.

--- inc/Statystyki.hh
+++ inc/Statystyki.hh
@@ -6,8 +6,14 @@ using namespace std;
 #include "LZespolona.hh"
 #include "WyrazenieZesp.hh"
 
+struct Zliczanie {
+
+  int Poprawne;
+  int Niepoprawne;
+};
 /*
  * Tu nalezy zdefiniowac funkcje, ktore definiuja model statystyki 
  */
+void Wyniktestu(Zliczanie Pkt);
 
 #endif
--- src/Statystyki.cpp
+++ src/Statystyki.cpp
@@ -5,3 +5,11 @@
  * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
  * w pliku naglowkowym.
  */
+void Wyniktestu(Zliczanie Pkt)
+{
+    double Wynik;
+    Wynik = ((double)Pkt.Poprawne / ((double)Pkt.Poprawne + (double)Pkt.Niepoprawne)) * 100;
+    cout << "Procentowa poprawność odpowiedzi: " << Wynik << "%" << endl;
+    cout << "Dobre odpowiedzi: " << Pkt.Poprawne << endl;
+    cout << "Złe odpowiedzi: " << Pkt.Niepoprawne << endl;
+}
